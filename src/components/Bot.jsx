6918762089f682b8768aba8d66ccd9cb95774f4c
Dants0 <gui.dg1@hotmail.com>
2022-07-05 20:00:06 -0300

    add readme

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -90,75 +90,80 @@ class Bot extends Component {
     render() {
         return (
             <ThemeProvider config={config} theme={theme}>
-                <ChatBot
-                    steps={[
-                        {
-                            id: '1',
-                            message: 'Qual é o seu nome?',
-                            trigger: 'name'
-                        },
-                        {
-                            id: 'name',
-                            user: true,
-                            trigger: '3'
-                        },
-                        {
-                            id: '3',
-                            message: 'Oi, {previousValue}! Qual desses te agrada mais?',
-                            trigger: 'cinema',
-                        },
-                        {
-                            id: 'cinema',
-                            options: [
-                                { value: 'Filme', label: 'Filme', trigger: '5' },
-                                { value: 'Anime', label: 'Anime', trigger: '5' },
-                                { value: 'Serie', label: 'Série', trigger: '5' }
-                            ]
-                        },
-                        {
-                            id: '5',
-                            message: 'Hum, notei que você tem preferência por {previousValue}. Agora me informa a sua idade.',
-                            trigger: 'idade'
-                        },
-                        {
-                            id: 'idade',
-                            user: true,
-                            trigger: '7',
-                            validator: (value) => {
-                                if (isNaN(value || id() === null)) {
-                                    return "Somente número";
-                                } else if (value < 0) {
-                                    return "Idade negativa? Fala sério cara. Hahaha"
-                                } else if (value > 120) {
-                                    return `${value}? Meteu essa`;
+                <div className="container">
+                    <div className="box">
+                        <h1>Chat Bot</h1>
+                    <ChatBot
+                        steps={[
+                            {
+                                id: '1',
+                                message: 'Qual é o seu nome?',
+                                trigger: 'name'
+                            },
+                            {
+                                id: 'name',
+                                user: true,
+                                trigger: '3'
+                            },
+                            {
+                                id: '3',
+                                message: 'Oi, {previousValue}! Qual desses te agrada mais?',
+                                trigger: 'cinema',
+                            },
+                            {
+                                id: 'cinema',
+                                options: [
+                                    { value: 'Filme', label: 'Filme', trigger: '5' },
+                                    { value: 'Anime', label: 'Anime', trigger: '5' },
+                                    { value: 'Serie', label: 'Série', trigger: '5' }
+                                ]
+                            },
+                            {
+                                id: '5',
+                                message: 'Hum, notei que você tem preferência por {previousValue}. Agora me informa a sua idade.',
+                                trigger: 'idade'
+                            },
+                            {
+                                id: 'idade',
+                                user: true,
+                                trigger: '7',
+                                validator: (value) => {
+                                    if (isNaN(value || id() === null)) {
+                                        return "Somente número";
+                                    } else if (value < 0) {
+                                        return "Idade negativa? Fala sério cara. Hahaha"
+                                    } else if (value > 120) {
+                                        return `${value}? Meteu essa`;
+                                    }
+                                    return true;
                                 }
-                                return true;
+                            },
+                            {
+                                id: '7',
+                                message: "Ok... Vamos checar seu sumário",
+                                trigger: 'review'
+                            },
+                            {
+                                id: 'review',
+                                component: <Review />,
+                                asMessage: true,
+                                delay: 2000,
+                                trigger: 'end-message'
+                            },
+                            {
+                                id: 'end-message',
+                                message: 'Obrigado, volte sempre!!',
+                                end: true
                             }
-                        },
-                        {
-                            id: '7',
-                            message: "Ok... Vamos checar seu sumário",
-                            trigger: 'review'
-                        },
-                        {
-                            id: 'review',
-                            component: <Review />,
-                            asMessage: true,
-                            delay: 2000,
-                            trigger: 'end-message'
-                        },
-                        {
-                            id: 'end-message',
-                            message: 'Obrigado, volte sempre!!',
-                            end: true
-                        }
-                    ]}
-                    headerTitle="Bip Bop 👾"
-                    placeholder="Digite sua mensagem"
-                    speechSynthesis={{ enable: true, lang: 'en' }}
-                    botAvatar="../src/img/agent.svg"
-                    userAvatar="../src/img/user.svg"
-                />
+                        ]}
+                        headerTitle="Bip Bop 👾"
+                        placeholder="Digite sua mensagem"
+                        speechSynthesis={{ enable: true, lang: 'en' }}
+                        botAvatar="../src/img/agent.svg"
+                        userAvatar="../src/img/user.svg"
+                    />
+                    </div>
+                </div>
             </ThemeProvider>
         );
     }
